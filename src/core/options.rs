77dd2c2f38905b1bfa0d2875deebd8ed43909ef9
Andrew Artz <9844465+keyboardsmoke@users.noreply.github.com>
2025-04-10 08:27:53 -0400

    * Set some options * Added some tests

--- src/core/options.rs
+++ src/core/options.rs
@@ -1,5 +1,13 @@
 
 pub fn setup_options(env: &mut minijinja::Environment) {
-    env.set_recursion_limit(500);   
+    // Set recursion limit
+    env.set_recursion_limit(500);
+    // Debug mode
     env.set_debug(true);
+    // Remove leading spaces and tabs from the start of a line to a block.
+    env.set_lstrip_blocks(true);
+    // Remove the first newline after a block.
+    env.set_trim_blocks(true);
+    // Complains very quickly about undefined values.
+    env.set_undefined_behavior(minijinja::UndefinedBehavior::Strict);
 }
